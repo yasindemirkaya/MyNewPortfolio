151cb6872f2c3a08a9473ab5fc31ad7d9964bb0f
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-12-28 22:58:03 +0300

    landing-page
    landing page created.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,16 @@
+import { Poppins } from 'next/font/google';
+import '../styles/globals.css'; // Global stilleri buraya dahil edin
+import './index.module.scss'; // Modül stilleri burada
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
